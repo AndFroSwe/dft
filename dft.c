6be199697f389aed8538047399eaa1eae318249a
andfroswe <gummianka88@gmail.com>
2017-08-30 15:30:42 +0200

    print and add works for circular buffer. changed printing to fifo

--- dft.c
+++ dft.c
@@ -13,16 +13,16 @@
 typedef struct sample_data
 {
     /* Circular buffer, design to efficiently hold data used to calc DFT. 
-     * LIFO implementation.
+     * FIFO implementation.
      */
     double *buffer;     // Start of data buffer
     double *buffer_end; // End of data buffer
     double *data_end;   // End of ring buffer
     unsigned int size;  // Size of data buffer (number of samples)
-    unsigned int fs;    // Sample rate [Hz]
+    double fs;    // Sample rate [Hz]
 } sample_data;
 
-void init_sample_data(sample_data *sd, unsigned int size, unsigned int fs)
+void init_sample_data(sample_data *sd, unsigned int size, double fs)
 {
     sd->buffer = malloc(sizeof(double)*size);
     sd->buffer_end = sd->buffer + size;
@@ -42,7 +42,7 @@ void add_data(sample_data *sd, double d)
     *sd->data_end = d; // Add data
     (sd->data_end)++; // Increase index of buffer
     // Check if buffer filled
-    if (sd->data_end == sd->buffer_end)
+    if (sd->data_end == (sd->buffer_end))
     {
         sd->data_end = sd->buffer;
     }
@@ -53,7 +53,6 @@ void calculate_dft(sample_data *sd, complex *result)
 {
     /* Calculates dft from sample data
      */
-    ! Test this function!
     int N = sd->size;
     double *p = sd->data_end;
 
@@ -64,8 +63,8 @@ void calculate_dft(sample_data *sd, complex *result)
             result[n] += *(--p)*cexp(-I*2*PI/N*n*k);
             k++;
             // Wrap around guard
-            if (p == sd->buffer) {
-                p = sd->buffer_end;
+            if (p == sd->buffer_end + 1) {
+                p = sd->buffer;
             }
         } while(p != sd->data_end);
     }
@@ -75,25 +74,32 @@ void print_data(const sample_data *sd)
     double *p = sd->data_end;
     printf("{");
     do {
-        printf("%0.2f ", *(--p));
-        if (p == sd->buffer) {
-            p = sd->buffer_end;
+        printf("%0.2f ", *p);
+        ++p;
+        if (p == (sd->buffer_end)) {
+            p = sd->buffer;
         }
     } while(p != sd->data_end);
     printf("}\n");
 }
 
+double generate_signal(double t)
+{
+    return 5 + 2*cos(2*PI*t - PI/2) + 3*cos(4*PI*t);
+    /*return 21 + 2*cos(2.5*2*PI*t - PI/2) + 5*sin(0.25*4*PI*t) + 30*cos(120*2*PI*t);*/
+}
 int main(int argc, char *argv[])
 {
-    unsigned int s = 32;
+    unsigned int s = 4;
+    double hz = 4.0;
     printf("Create\n");
     printf("Init\n");
     sample_data *sd = malloc(sizeof(sample_data));
-    init_sample_data(sd, s, 1);
-    printf("Size: %d \n", sd->size);
+    init_sample_data(sd, s, hz);
     printf("Add\n");
-    for (int i = 0; i < s + 100; ++i) {
-        add_data(sd, i);
+    for (int i = 0; i < s; ++i) {
+        printf("%f\n",generate_signal(i/hz));
+        add_data(sd, generate_signal(i/hz));
     }
 
     print_data(sd);
