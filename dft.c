bc2d9878046f9bb022cf043b2317466bef4710b8
andfroswe <gummianka88@gmail.com>
2017-08-30 06:09:42 +0200

    Test version where dft is added to data struct

--- dft.c
+++ dft.c
@@ -8,11 +8,15 @@
 #include <math.h>
 #include <stdlib.h>
 
+#define PI 3.141592654
+
 typedef struct sample_data
 {
+    /* Circular buffer, design to efficiently hold data used to calc DFT. 
+     * LIFO implementation.
+     */
     double *buffer;     // Start of data buffer
     double *buffer_end; // End of data buffer
-    double *data_start; // Start of ring buffer
     double *data_end;   // End of ring buffer
     unsigned int size;  // Size of data buffer (number of samples)
     unsigned int fs;    // Sample rate [Hz]
@@ -22,7 +26,6 @@ void init_sample_data(sample_data *sd, unsigned int size, unsigned int fs)
 {
     sd->buffer = malloc(sizeof(double)*size);
     sd->buffer_end = sd->buffer + size;
-    sd->data_start = sd->buffer;
     sd->data_end = sd->buffer;
     sd->size = size;
     sd->fs = fs;
@@ -46,6 +49,27 @@ void add_data(sample_data *sd, double d)
     
 }
 
+void calculate_dft(sample_data *sd, complex *result)
+{
+    /* Calculates dft from sample data
+     */
+    ! Test this function!
+    int N = sd->size;
+    double *p = sd->data_end;
+
+    for (int n = 0; n < N; ++n) {
+        result[n] = 0;
+        int k = 0;
+        do {
+            result[n] += *(--p)*cexp(-I*2*PI/N*n*k);
+            k++;
+            // Wrap around guard
+            if (p == sd->buffer) {
+                p = sd->buffer_end;
+            }
+        } while(p != sd->data_end);
+    }
+}
 void print_data(const sample_data *sd)
 {
     double *p = sd->data_end;
